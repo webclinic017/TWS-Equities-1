4bae60ad4b8f6bc3145600d186012e5119a252c5
Mandeep Singh <singh.mandeep2207@gmail.com>
2021-01-08 11:37:44 +0530

    updated TWS Client

--- tws_clients/data_extractor.py
+++ tws_clients/data_extractor.py
@@ -5,7 +5,6 @@
     =ArrayFormula(IF(ROW(A:A)=1, "ID", IF(B:B="", "", (ROW(B:B)-1))))
 """
 
-
 from tws_clients import TWSWrapper
 from tws_clients import TWSClient
 from helpers import create_stock
@@ -13,13 +12,16 @@ from helpers import get_logger
 from helpers import make_dirs
 from json import dumps
 from os.path import dirname
-from os.path import isfile
 from os.path import join
+from os import walk
 from os import name as os_name
+from itertools import chain
 import signal
-
+from time import sleep
 
 OS_IS_UNIX = os_name == 'posix'
+PROJECT_DIRECTORY = dirname(dirname(__file__))
+flatten = chain.from_iterable
 
 
 class HistoricalDataExtractor(TWSWrapper, TWSClient):
@@ -31,7 +33,10 @@ class HistoricalDataExtractor(TWSWrapper, TWSClient):
         TWSWrapper.__init__(self)
         TWSClient.__init__(self, wrapper=self)
         self.ticker = None
+        self._target_tickers = []
+        self._proccesed_tickers = []
         self.is_connected = False
+        self.directory_maker = make_dirs
         # TODO: implement broken connection handler
         self.connection_is_broken = False
         self.handshake_completed = False
@@ -48,21 +53,23 @@ class HistoricalDataExtractor(TWSWrapper, TWSClient):
         self.timeout = timeout
         self.create_data_dump = create_data_dump
         self.debug = debug
-        self.output_location = output_location or 'data_files/historical_data'
+        self.output_location = output_location or join(PROJECT_DIRECTORY,
+                                                       'data_files',
+                                                       'historical_data',
+                                                       self.end_date)
         log_level = 'DEBUG' if debug else ('INFO' if verbose else 'CRITICAL')
         self.logger = logger or get_logger(__name__, log_level)
         self.max_attempts = max_attempts
         self.data = None
 
-    def _init_data_tracker(self):
+    def _init_data_tracker(self, ticker):
         """
             Initializes the data tracker.
             Should be invoked for every new ticker.
         """
         _meta_data = {'start': None, 'end': None, 'status': False, 'attempt': 0, '_error_stack': [], 'output_file': ''}
         _initial_data = {'meta_data': _meta_data, 'bar_data': []}
-        self.logger.debug('Data tracker was initialized.')
-        return _initial_data
+        self.data[ticker] = _initial_data
 
     def _reset_attr(self, **kwargs):
         """
@@ -74,9 +81,11 @@ class HistoricalDataExtractor(TWSWrapper, TWSClient):
                 raise ValueError(f'Attribute {attr} can not be None.')
             setattr(self, attr, value)
 
-    def _request_historical_data(self):
+    def _set_timeout(self):
         """
-            Sends request to TWS API
+            Break the call running longer than timeout threshold.
+            Call error method with code=-1 on timeout.
+            NOTE: Not supported on Windows OS yet.
         """
         # noinspection PyUnusedLocal
         def _handle_timeout(signum, frame):
@@ -85,36 +94,75 @@ class HistoricalDataExtractor(TWSWrapper, TWSClient):
         if OS_IS_UNIX:
             signal.signal(signal.SIGALRM, _handle_timeout)
             signal.alarm(self.timeout)
-        self.logger.info(f'Requesting historical data for ticker: {self.ticker}')
-        contract = create_stock(self.ticker)
+
+    def _request_historical_data(self, ticker):
+        """
+            Sends request to TWS API
+        """
+        self._set_timeout()
+        contract = create_stock(ticker)
         end_date_time = f'{self.end_date} {self.end_time}'
-        self.data['meta_data']['attempt'] += 1
-        self.reqHistoricalData(self.ticker, contract, end_date_time, self.duration, self.bar_size, self.what_to_show,
+        self.data[ticker]['meta_data']['attempt'] += 1
+        self.logger.info(f'Requesting historical data for ticker: {ticker}')
+        print(f'Requesting historical data for ticker: {ticker}')
+        self.reqHistoricalData(ticker, contract, end_date_time, self.duration, self.bar_size, self.what_to_show,
                                self.use_rth, self.date_format, self.keep_upto_date, self.chart_options)
 
-    def _create_data_dump(self):
+    def _create_data_dump(self, ticker, data):
         """
             Evaluates extraction status and saves ticker data to relevant file.
+            :param ticker: ticker ID
+            :param data: historical data in a dictionary
         """
+        print(f'Creating data dump for: {ticker}')
+        print(f'Data:\n{dumps(data, indent=2, sort_keys=True)}')
         if self.create_data_dump:
-            status_directory = 'success' if self.data['meta_data']['status'] else 'failure'
-            target_directory = join(dirname(dirname(__file__)), self.output_location,
-                                    self.end_date, status_directory)
-            target_file = join(target_directory, f'{self.ticker}.json')
-            # create relevant directories
-            make_dirs(target_directory)
-            if not(isfile(target_file)):
-                self.save_ticker_file(self.data, target_file)
-            self.data['meta_data']['output_file'] = target_file
+            status_directory = 'success' if data['meta_data']['status'] else 'failure'
+            target_directory = join(self.output_location, status_directory)
+            target_file = join(target_directory, f'{ticker}.json')
+            self.directory_maker(target_directory)
+            # if not (isfile(target_file)):
+            self.data[ticker]['meta_data']['output_file'] = target_file
+            self.save_ticker_file(data, target_file)
 
-    def _extraction_check(self):
+    def _extraction_check(self, ticker):
         """
             Returns True if:
                 - Bar data has been extracted and saved to success location
                                     OR
                 - Error data has been extracted and saved to failure location
         """
-        return isfile(self.data['meta_data']['output_file'])
+        meta_data = self.data[ticker]['meta_data']
+        return meta_data['status'] or meta_data['attempt'] >= self.max_attempts
+
+    def _get_processed_tickers(self):
+        """
+            Read output location and findout tickers for which data dump has been created.
+            :return: list of processed tickers
+        """
+        # get all tickers for which a data dump has been created
+        # success & failure directories, both are included
+        # get all files from self.output_location
+        # filter out files ending with .json
+        # split filename and convert to int, to get ticker ID
+        return list(
+                        map(lambda z: int(z.split('.')[0]),
+                            filter(lambda y: y.endswith('.json'),
+                                   flatten(map(lambda x: x[2],
+                                               walk(self.output_location)
+                                               )
+                                           )
+                                   )
+                            )
+                    )
+
+    def _get_target_ticker(self):
+        """
+            :return: First ticker from unprocessed tickers
+        """
+        unprocessed_tickers = list(set(self._target_tickers).difference(self._get_processed_tickers()))
+        if bool(unprocessed_tickers):
+            return unprocessed_tickers[0]
 
     def save_ticker_file(self, data, file_path):
         """
@@ -122,7 +170,7 @@ class HistoricalDataExtractor(TWSWrapper, TWSClient):
             @param data: dictonary containing ticker's historical data or error info
             @param file_path: target file where data is to be saved
         """
-        indent = None  # 4 if self.debug else None
+        indent = 4 if self.debug else 2
         with open(file_path, 'w') as f:
             f.writelines(dumps(data, sort_keys=True, indent=indent))
             self.logger.info(f'Written ticker data at location: {file_path}')
@@ -139,7 +187,6 @@ class HistoricalDataExtractor(TWSWrapper, TWSClient):
             super().connect(host, port, client_id)
             self.is_connected = self.isConnected()
             self.run()
-            self.logger.info(f'Client successfully connected to TWS API')
 
     def disconnect(self):
         """
@@ -159,67 +206,81 @@ class HistoricalDataExtractor(TWSWrapper, TWSClient):
             raise ConnectionError(f'Not yet connected to TWS API, invoke "connect" method with valid arguments.')
         super().run()
 
-    def extract_historical_data(self, ticker=None):
+    def extract_historical_data(self, tickers=None):
         """
-            Performs historical data extraction on tickers provided at the time of initialization
-            User must first connect to TWS by invoking "connect" method
+            Performs historical data extraction on tickers provided as input.
         """
-        if ticker is not None:
-            self._reset_attr(ticker=ticker, data=self._init_data_tracker(), extraction_completed=False)
+        if tickers is not None:
+            self._reset_attr(_target_tickers=tickers, data={}, extraction_completed=False)
         if not self.is_connected:
             self.connect()
-        if not self.extraction_completed and self.data['meta_data']['attempt'] < self.max_attempts:
-            self.logger.info(f'Extracting data for: {ticker}')
-            self._request_historical_data()
+        # _target_ticker = self._get_target_ticker()
+        unprocessed_tickers = list(set(self._target_tickers).difference(self._proccesed_tickers))
+        print(f'UU: {unprocessed_tickers}')
+        if bool(unprocessed_tickers):
+            _target_ticker = unprocessed_tickers[0]
+            if _target_ticker not in self.data:
+                self._init_data_tracker(_target_ticker)
+            ticker_is_not_processed = not self._extraction_check(_target_ticker)
+            self.logger.info(f'Extracting data for: {_target_ticker}')
+            print(f'Extracting data for: {_target_ticker} | Type: {type(_target_ticker)}')
+            if ticker_is_not_processed:
+                self._request_historical_data(_target_ticker)
+            else:
+                self._create_data_dump(_target_ticker, self.data[_target_ticker])
+                self._proccesed_tickers.append(_target_ticker)
+                self.extract_historical_data()
         else:
-            self._create_data_dump()
-            self.extraction_completed = True
+            print(f'Ran out of tickers to process...')
             self.disconnect()
 
     def historicalData(self, id, bar):
         """
             This method is receives data from TWS API, invoked automatically after "reqHistoricalData".
-            @param id: respresents ticker ID
-            @param bar: a bar object that contains OHLCV data for a given timestamp
+            :param id: respresents ticker ID
+            :param bar: a bar object that contains OHLCV data
         """
-        bar = {'time_stamp': bar.date, 'open': bar.open, 'high': bar.high,
-               'low': bar.low, 'close': bar.close, 'volume': bar.volume,
-               'average': bar.average, 'count': bar.barCount,
-               'session': 1 if int(bar.date.split(' ')[-1].split(':')[0]) < 12 else 2}
-        self.data['bar_data'].append(bar)
+        session = 1 if int(bar.date.split(' ')[-1].split(':')[0]) < 12 else 2
+        bar = {'time_stamp': bar.date, 'open': bar.open, 'high': bar.high, 'low': bar.low,
+               'close': bar.close, 'volume': bar.volume, 'average': bar.average,
+               'count': bar.barCount, 'session': session}
+        self.data[id]['bar_data'].append(bar)
 
     def historicalDataEnd(self, id, start, end):
         """
             This method is called automatically after all the bars have been generated by "historicalData".
             Marks the completion of historical data generation for a given ticker.
-            @param id: ticker ID
-            @param start: starting timestamp
-            @param end: ending timestamp
+            :param id: ticker ID
+            :param start: starting timestamp
+            :param end: ending timestamp
         """
         self.logger.info(f'Data extraction completed for ticker: {id}')
-        self.data['meta_data']['start'] = start
-        self.data['meta_data']['end'] = end
-        self.data['meta_data']['status'] = True
-        self._create_data_dump()
-        self.extraction_completed = True
-        self.disconnect()
+        self.data[id]['meta_data']['start'] = start
+        self.data[id]['meta_data']['end'] = end
+        self.data[id]['meta_data']['status'] = True
+        self._create_data_dump(id, self.data[id])
+        self._proccesed_tickers.append(id)
+        sleep(0.1)
+        self.extract_historical_data()
+        # self.extraction_completed = True
+        # self.disconnect()
 
     def error(self, id, code, message):
         """
             Error handler for all API calls, invoked directly by EClient methods
-            @param id: error ID (-1 means no informational message, not true error)
-            @param code: error code, defines error type
-            @param message: error message, information about error
+            :param id: error ID (-1 means no informational message, not true error)
+            :param code: error code, defines error type
+            :param message: error message, information about error
         """
         # -1 is not a true error, but only an informational message
         # initial call to run invokes this method with error ID = -1
-        # print(f'Error: ID: {id} | Code: {code} | String: {message}')
+        print(f'Error: ID: {id} | Code: {code} | String: {message}')
         if id == -1:
             self.logger.info(f'{message}')
             # error code 502 indicates connection failure
             if code == 502:
                 self.logger.critical(f'Connection Failure: {message}, Error Code: {code}')
-                raise ConnectionError('Could not connect to TWS, please ensure TWS is running with proper API settings.')
+                raise ConnectionError('Could not connect to TWS, please ensure TWS is running.')
             # error codes 2103, 2105, 2157 indicate broken connection
             if code in [2103, 2105, 2157]:
                 self.logger.critical(f'Insecure Connection: {message}, Error code: {code}')
@@ -233,17 +294,22 @@ class HistoricalDataExtractor(TWSWrapper, TWSClient):
                 self.logger.info(f'Secure connection established to TWS API, initial handshake completed.')
                 self.handshake_completed = True
         else:
+            print(f'\n\n------------------\n\n')
             self.logger.error(f'{message}: Ticker ID: {id}, Error Code: {code}')
-            meta_data = self.data['meta_data']
+            meta_data = self.data[id]['meta_data']
             attempts = meta_data['attempt']
 
             if attempts <= self.max_attempts:
                 error = {'code': code, 'message': message}
                 meta_data['_error_stack'].append(error)
             else:
-                if not self.extraction_completed:
-                    self._create_data_dump()
-                    self.extraction_completed = True
+                self._create_data_dump(id, self.data[id])
+                self._proccesed_tickers.append(id)
+                sleep(0.1)
+                # self.cancelHistoricalData(id)
+                # if not self.extraction_completed:
+                # self._create_data_dump()
+                # self.extraction_completed = True
 
             # -1 indicates a timeout
             # 162 indicates that HMDS return no data
@@ -253,39 +319,41 @@ class HistoricalDataExtractor(TWSWrapper, TWSClient):
                 self.cancelHistoricalData(id)
                 self.logger.warning(f'Canceling: {id} | {code} | {message}')
 
+        self.extraction_completed = self._target_tickers == self._proccesed_tickers
         if self.handshake_completed:
             if not self.extraction_completed:
                 self.extract_historical_data()
             else:
                 self.disconnect()
 
-    def get_data(self):
-        return self.data
-
 
 if __name__ == '__main__':
     from time import time
     from data_files.input_data import test_tickers
 
-    test_tickers = test_tickers[:5]
+    test_tickers = test_tickers[:15]
+    print(f'Target: {test_tickers}')
     total_tickers = len(test_tickers)
     start = time()
     data = {}
     extractor = HistoricalDataExtractor(end_date='20210106',
                                         end_time='09:01:00',
                                         create_data_dump=True,
-                                        timeout=3,
+                                        timeout=3000,
                                         debug=True)
-    for i in range(total_tickers):
-        ticker = test_tickers[i]
-        extractor = HistoricalDataExtractor(end_date='20210106',
-                                            end_time='09:01:00',
-                                            create_data_dump=True,
-                                            timeout=3,
-                                            debug=True)
-        extractor.extract_historical_data(ticker)
-        data[ticker] = extractor.data
-        print(f'=== Processed: {i+1} / {total_tickers} ===')
+    extractor.extract_historical_data(test_tickers)
+    # print(dumps(extractor.data, indent=2, sort_keys=True))
+
+    # for i in range(total_tickers):
+    #     ticker = test_tickers[i]
+    #     extractor = HistoricalDataExtractor(end_date='20210106',
+    #                                         end_time='09:01:00',
+    #                                         create_data_dump=True,
+    #                                         timeout=3,
+    #                                         debug=True)
+    # extractor.extract_historical_data(ticker)
+    # data[ticker] = extractor.data
+    # print(f'=== Processed: {i+1} / {total_tickers} ===')
     end = time()
     lapsed = round(end - start, 3)
     print(dumps(data, indent=2, sort_keys=True))
